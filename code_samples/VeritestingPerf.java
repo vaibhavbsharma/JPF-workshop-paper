1ffecbd297a868d1531032993c5529aeed992926
Vaibhav Sharma <vaibhav@umn.edu>
2017-08-04 15:36:16 -0500

    Rewriting Intro for new example + shortening Intro

--- code_samples/VeritestingPerf.java
+++ code_samples/VeritestingPerf.java
@@ -1,6 +1,6 @@
 // x = array of symbolic integers
 // len = concrete length of x
-for (int i=0; i < len; i++) {
+for (int i = 0; i < len; i++) {
   // Begin region for static unrolling
   if (x[i] < 0) sum += -1;
   else if (x[i] > 0) sum += 1;
