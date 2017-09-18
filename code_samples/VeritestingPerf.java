d12bd82b37ed16b10d934fefd861b6888a700da1
Vaibhav Sharma <vaibhav@umn.edu>
2017-09-18 17:28:41 -0500

    Addressing reviewer #1's concerns
    This causes the paper to overflow into page 6. I will come back to
    fixing this at the end.

--- code_samples/VeritestingPerf.java
+++ code_samples/VeritestingPerf.java
@@ -1,11 +1,13 @@
 // x = array of symbolic integers
 // len = concrete length of x
+sum = 0;
 for (int i = 0; i < len; i++) {
   // Begin region for static unrolling
   if (x[i] < 0) sum += -1;
   else if (x[i] > 0) sum += 1;
   // End region for static unrolling
 }
-if (sum < 0) System.out.println("neg");
-else if (sum > 0) System.out.println("pos");
-else System.out.println("bug");
+if (sum < 0) { // handles negative sum
+} else if (sum > 0) { // handles positive sum
+} else { // contains a bug
+}
